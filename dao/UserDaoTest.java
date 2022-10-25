6b8aff5e115e3e7556360056860ae5a7f15d1447
heejunns <107148343+heejunns@users.noreply.github.com>
2022-10-24 19:12:58 -0700

    connection 분리

---
+++ dao/UserDaoTest.java
@@ -0,0 +1,23 @@
+package com.likelion.dao;
+
+import com.likelion.domain.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @Test
+    void  addAndSelect(){
+        UserDao userDao = new UserDao();
+        String id = "39";
+        userDao.add(new User(id,"hihi","123456"));
+
+        User user = userDao.findById(id);
+        System.out.println(user.getPassword());
+
+        Assertions.assertEquals("hihi",user.getName());
+    }
+
+}
